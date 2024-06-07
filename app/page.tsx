598bc6d7388296465b41d4b625cc10afcb54924d
mxzzmaih <muzammill78646@gmail.com>
2024-06-07 12:29:31 +0530

    stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import DashBoardCard from "@/components/dashboard/DashBoardCard";
+import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 export default function Home() {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-between gap-5 mb-5"></div>
-       <DashBoardCard />
+       <DashBoardCard title="Posts" count = {100} icon ={<Newspaper className="text-slate-600 dark:text-slate-400 w-12 h-12 size={72}" />}/>
+       <DashBoardCard title="Categories" count = {12} icon ={<Folder className="text-slate-600 dark:text-slate-400 w-12 h-12 size={72}" />}/>
+       <DashBoardCard title="Users" count = {750} icon ={<User className="text-slate-600 dark:text-slate-400 w-12 h-12 size={72}" />}/>
+       <DashBoardCard title="Comments" count = {1200} icon ={<MessageCircle className="text-slate-600 dark:text-slate-400 w-12 h-12 size={72}" />}/>
     </>
   );
 }
--- components/dashboard/DashBoardCard.tsx
+++ components/dashboard/DashBoardCard.tsx
@@ -1,14 +1,25 @@
 import { Card, CardContent } from "@/components/ui/card"
-import { Newspaper } from "lucide-react";
-const DashBoardCard = () => {
-    return ( <Card className="bg-slate-100 dark:bg-slate-800 p-400 pb-100">
-        <CardContent>
-            <h3 className="text-3xl text-cnter mb-4 font-bold text-slate-500 dark:text-slate-200">Posts</h3>
-            <div className="flex gap-5 justify-center items-center">
-                <Newspaper className="text-slate-500 size={1000}"/>
-            </div>
-        </CardContent>
-    </Card> );
+
+import { LucideIcon } from "lucide-react";
+
+interface DashBoardCard{
+    title: string;
+    count: number;
+    icon: React.ReactElement<LucideIcon>
 }
- 
+
+const DashBoardCard = ({ title, count, icon }: DashBoardCard) => {
+    return (
+        <Card className="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-lg max-w-md mx-auto">
+            <CardContent>
+                <h3 className="text-3xl text-center mb-4 font-bold text-slate-800 dark:text-slate-400">{title }</h3>
+                <div className="flex gap-5 justify-center items-center">
+                    {icon}
+                    <h3 className="text-5xl font-semibold text-slate-800 dark:text-slate-400">{count}</h3>
+                </div>
+            </CardContent>
+        </Card>
+    );
+}
+
 export default DashBoardCard;
