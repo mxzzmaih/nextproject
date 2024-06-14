4788f6a96fa19b9a8814bef3f7ef4400fe6a9228
mxzzmaih <muzammill78646@gmail.com>
2024-06-14 16:20:44 +0530

    done with it

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Navbar from "@/components/ui/Navbar";
-import Sidebar from "@/components/ui/Sidebar";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { Toaster } from '@/components/ui/toaster';
+import { ThemeProvider } from '@/components/providers/ThemeProvider';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Muzammil AD",
-  description: "Admin DashBoard",
+  title: 'MS Dashboard',
+  description: 'Admin dashboard',
 };
 
 export default function RootLayout({
@@ -17,13 +17,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[200ch] w-[300px]"><Sidebar /></div>
-          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-        </div>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem={true}
+          storageKey='dashboard-theme'
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
